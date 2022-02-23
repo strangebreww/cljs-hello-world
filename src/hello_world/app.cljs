97835c7333349c6adf403d7d2e5bc0df4fb39fd5
Alexey Ermakov <strangebrew@yandex.ru>
2022-02-23 13:44:33 +0300

    add webpack and axios

--- src/hello_world/app.cljs
+++ src/hello_world/app.cljs
@@ -1,7 +1,11 @@
-(ns ^:figwheel-hooks hello-world.app)
+(ns ^:figwheel-hooks hello-world.app
+  (:require [axios]))
+
+(-> (.. axios (get "https://jsonplaceholder.typicode.com/todos/1"))
+    (.then #(js/console.log %)))
 
 (defn ^:after-load re-render []
-      (js/console.log "Hello, World... again"))
+  (js/console.log "Hello, World... again"))
 
 (js/console.log "Hello, World!")
 
